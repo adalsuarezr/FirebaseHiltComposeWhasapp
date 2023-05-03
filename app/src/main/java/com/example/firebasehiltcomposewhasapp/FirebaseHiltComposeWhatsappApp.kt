0d38d40716943b43c3eb311e9d457c9b13397e2e
adalsuarezr <adalsuarezr@gmail.com>
2023-05-03 10:03:00 +0100

    Initial commit

---
+++ app/src/main/java/com/example/firebasehiltcomposewhasapp/FirebaseHiltComposeWhatsappApp.kt
@@ -0,0 +1,9 @@
+package com.example.firebasehiltcomposewhasapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class FirebaseHiltComposeWhatsappApp: Application() {
+    
+}
